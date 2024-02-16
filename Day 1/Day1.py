5c59f2a2ea654958c4a87ad3c5e30616b45061bc
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-02-16 12:02:01 +0530

    Day - 1

---
+++ Day 1/Day1.py
@@ -0,0 +1,49 @@
+# Learning about Variables/Containers:
+x = 5
+y = 10
+print(x+y) # print is fuction/command used to show something on output screen/terminal
+
+print("Hello Navdeep")
+# Any Thing Written inside "" Is called String.
+
+
+num1 = 5
+num2 = 10
+print("The Answer Is =",num1+num2)
+
+a = 5
+b = 4.4
+c = "Satyam"
+d = True
+
+# 5,3,-5                    -> Integers
+# 3.5,-5.5                  -> Decimal/Float
+# "Any thing Inside This"   -> String
+# True/False                -> Boolean
+
+x = 41
+y = 5
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x//y)
+print(x*y)
+print(x%y) # % is called Modulo
+
+print("---------------")
+
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+print(x == y)
+print(x != y)
+
+print("-------------")
+
+
+name = input()
+print("Hello",name)
+
+
